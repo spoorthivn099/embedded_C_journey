6db3c66926c10300b642449afcc2e7d6bb49f9ee
Spoorthi V N <spoorthivn033@gmail.com>
2026-08-30 20:45:39 +0530

    Create Even_Odd.c to check even numbers
    The program is intended to check and print even numbers from 0 to 10.

---
+++ C_fundamentals /Day2_Conditional_statement /Even_Odd.c
@@ -0,0 +1,10 @@
+/*Write a C program to input an integer and check whether it is even or odd.*/
+
+#include <stdio.h>
+int main()
+{
+	int num;
+	for (num=0;num<=10;num+=2)
+	printf("%d\n",Num);
+	return 0;
+}
